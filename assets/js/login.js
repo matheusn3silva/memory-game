75fa84e6d43f64d33573c683d2d19bce5812e8eb
Matheus Nasc <matheusnascimentox725@gmail.com>
2024-03-06 09:44:53 -0300

    Add form function and create Game page

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,8 +1,20 @@
 const input = document.querySelector(".login__input");
 const button = document.querySelector(".login__button");
+const form = document.querySelector(".login__form");
 
 const validateInput = ({ target }) => {
-  target.value.length > 3 ? button.removeAttribute("disabled") : button.setAttribute("disabled", "");
+  target.value.length > 3
+    ? button.removeAttribute("disabled")
+    : button.setAttribute("disabled", "");
+};
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+
+  localStorage.setItem("player", input.value);
+
+  window.location = "pages/game.html";
 };
 
 input.addEventListener("input", validateInput);
+form.addEventListener("submit", handleSubmit);
